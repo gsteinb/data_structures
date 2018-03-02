08aeca3abdae310a2456425a49a5cb1567c0e188
gsteinb <gerrit_steinbach@hotmail.com>
2018-03-02 12:25:38 -0500

    Added a min heap implementation using arrays in python

---
+++ min_heap.py
@@ -0,0 +1,66 @@
+class MinHeap:
+    def __init__(self, heap=[]):
+        if heap == []:
+            # storing the heap size here
+            self.heap = [0]
+        else:
+            self.heap = [0] + heap
+            i = len(self.heap)
+            self.heap[0] = i # get the size of the heap
+            while i > 0:
+                self.heapify(i)
+                i -= 1
+
+    def __str__(self):
+        return str(self.heap[1:]) + " Heap size: {}".format(self.heap[0])
+        
+    def insert(self, val):
+        self.heap.append(val)
+        self.heap[0] += 1
+        index = self.heap[0] # since we are storing the length
+        while (val < self.heap[index//2] and index != 1):
+            # store as a temp value
+            temp = self.heap[index//2]
+            self.heap[index//2] = val
+            self.heap[index] = temp
+            index = index // 2
+            
+    def extract_min(self):
+        min_value = self.heap[1]
+        # decrease heap size
+        self.heap[0] -= 1
+        self.heap[1] = self.heap.pop() # get the value from the right-most leaf
+        self.heapify(1) # heapify at index 1
+        return min_value
+        
+    def heapify(self, i):
+        heap_size = self.heap[0]
+        left = 2*i
+        right = 2*i + 1
+        min_value = i
+        # make sure that left and right are in the limits of the heap array
+        if (left <= heap_size-1 and self.heap[left] < self.heap[i]):
+            min_value = left
+        if (right <= heap_size-1 and self.heap[right] < self.heap[min_value]):
+            min_value = right
+        if (min_value != i):
+            temp = self.heap[min_value]
+            self.heap[min_value] = self.heap[i]
+            self.heap[i] = temp
+            self.heapify(min_value)
+            
+
+if __name__ == "__main__":    
+    a = MinHeap()
+    a.insert(8)
+    print(a)
+    a.insert(1)
+    print(a)
+    a.insert(3)
+    print(a)
+    a.insert(4)
+    print(a)
+    print(a.extract_min())
+    print(a)
+    b = MinHeap([4,3,5,8])
+    print(b)
