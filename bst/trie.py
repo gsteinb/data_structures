c2d32e35d4989556abc08eea71078469110ac914
gsteinb <gerrit_steinbach@hotmail.com>
2018-04-12 14:40:22 -0400

    Added a trie data structure

--- bst/trie.py
+++ bst/trie.py
@@ -1,24 +1,44 @@
+"""
+Program for a challenge at hackerrank.com
+We're going to make our own Contacts application! The application must perform two types of operations:
+
+add name, where name is a string denoting a contact name. This must store name as a new contact in the application.
+find partial, where partial is a string denoting a partial name to search the application for. 
+It must count the number of contacts starting with partial and print the count on a new line.
+"""
 class Node:
+    """Node class"""
     def __init__(self):
         self.children = {}
         self.valid = 0
+        # store the number of children so we can count the number of
+        # valid words faster
         self.num_children = 0
         
 class Trie:
+    """ a trie class, that allows insertion and lookup """
     def __init__(self):
         self.root = Node()
 
     def insert(self, name):
+        ''' (self, str) -> None
+        inserts a name as a trie
+        '''
         current = self.root
         for letter in name:
             if current.children.get(letter) == None:
                 node = Node() # create a new node
                 current.children[letter] = node
+            # update the number of children
             current.num_children += 1
             current = current.children[letter]
         current.valid = 1
         
     def search(self, name):
+        """(self, str) -> int
+        searches based on a string and returns
+        the number of children that belong to the
+        partial string """
         result = 0
         current = self.root
         for letter in name:
@@ -32,6 +52,7 @@ class Trie:
         
         
 if __name__ == "__main__":
+    # main function
     tries = Trie()
     num = int(input().strip('\n'))
     for i in range(num):
